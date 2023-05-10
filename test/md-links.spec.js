865b6c796c8b4bff096bda56cccb201c17943f21
daiane-dosanjos <dayanjos01@gmail.com>
2023-05-09 21:09:31 -0300

    refatorando testes

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -81,25 +81,25 @@ const statsValidateLinksArray = {
 
 
 describe('MdLinks', () => {
-  test('com caminho absoluto, deve retornar uma promessa', ()=>{
+  it('com caminho absoluto, deve retornar uma promessa', ()=>{
     mdLinks("./arquivos").then((res) =>{
      expect(res).toEqual(defaultArray);
     });
   })
   
-  test('deve retornar um erro se o caminho for inválido', ()=>{
+  it('deve retornar um erro se o caminho for inválido', ()=>{
       expect(mdLinks("arquivos")).rejects.toMatch("❌ Caminho inválido, digite um caminho válido. "); //para decodificar o motivo de uma promessa rejeitada
   });
 
-  test('se não tiver arquivos md deve rejeitar "sem arquivos md"', ()=>{
+  it('se não tiver arquivos md deve rejeitar "sem arquivos md"', ()=>{
       expect(mdLinks("C:\\Users\\dayan\\Desktop\\LAB\\md-links\\arquivos\\arquivo.txt")).rejects.toMatch('sem arquivos md');
   });
 
-  test('se não tiver arquivos md deve rejeitar "sem arquivos md"', ()=>{
+  it('se não tiver arquivos md deve rejeitar "sem arquivos md"', ()=>{
       expect(mdLinks("./arquivos/arquivo.md" , {validate: true})).rejects.toMatch('sem arquivos md');
   });
 
-  test('com caminho relativo, deve retornar uma promessa', ()=> {
+  it('com caminho relativo, deve retornar uma promessa', ()=> {
     mdLinks("./arquivos/arquivo.md").then((res) =>{
      expect(res).toEqual(defaultArray);
     });
@@ -107,7 +107,7 @@ describe('MdLinks', () => {
 });
 
 describe('MdLinks com --validate', ()=>{
-   test('deve retornar um objeto com href,file,status e Ok o Fail message', ()=>{
+   it('deve retornar um objeto com href,file,status e Ok o Fail message', ()=>{
     mdLinks("./arquivos/arquivo.md" , {validate: true}).then((res)=>{
       expect(res).toEqual(arrayValidate);
     });
@@ -115,23 +115,23 @@ describe('MdLinks com --validate', ()=>{
 });
 
 describe('getUniqueLinks function', ()=>{
-   test('deve retornar o número de links únicos', ()=>{
+   it('deve retornar o número de links únicos', ()=>{
     expect(getUniqueLinks(uniqueLinksArray)).toBe(2);
    });
 });
 
 describe('statsLinks deve retornar o total de links', ()=>{
-  test('uma matriz de 2 links, deve retornar 2 links exclusivo', ()=>{
+  it('uma matriz de 2 links, deve retornar 2 links exclusivo', ()=>{
     expect(statsLinks(uniqueLinksArray)).toEqual(statsLinksArray);
   });
 
-    test('ValidStatsLinks deve retornar os links totais, únicos e quebrados', ()=>{
+    it('ValidStatsLinks deve retornar os links totais, únicos e quebrados', ()=>{
       expect(validateStatsLinks(uniqueLinksArray)).toEqual(statsValidateLinksArray);
     });
 });
 
 describe('function httpLinks', ()=>{
-   test('retorna uma promessa com links validados', ()=>{
+   it('retorna uma promessa com links validados', ()=>{
     httpLinks(defaultArray).then((res) =>{
       expect(res).toEqual(arrayValidate);
     });
@@ -140,25 +140,25 @@ describe('function httpLinks', ()=>{
 
 
 describe('getAbsolutePath', () => {
-    test('caminho relativo convertido em caminho absoluto', () => {
+    it('caminho relativo convertido em caminho absoluto', () => {
       expect(getAbsolutePath(relativePath)).toBe("C:\\Users\\dayan\\Desktop\\LAB\\md-links\\arquivos");
     });
 
-    test('caminho absoluto permanece o mesmo', () => {
+    it('caminho absoluto permanece o mesmo', () => {
       expect(getAbsolutePath(absolutePath)).toBe(absolutePath);
     });
 
-    test('Caminho vazio retorna falso', ()=>{
+    it('Caminho vazio retorna falso', ()=>{
       expect(getAbsolutePath("")).toBeFalsy()
     })
 });
 
 describe('getFiles e MdFiles', ()=>{
-  test('deve retornar o array de arquivos', ()=>{
+  it('deve retornar o array de arquivos', ()=>{
     expect(getFiles(absolutePath)).toEqual(files)
   })
 
-  test('deve retornar a matriz de arquivos md', ()=>{
+  it('deve retornar a matriz de arquivos md', ()=>{
     expect(getMdFiles(files)).toEqual(mdFiles)
   })
 })
